d433919dd9abb4c07ee95b7a0a95377a8aa3b74b
michalgebauer <michal.gebauer@gmail.com>
2018-02-10 22:37:17 +0100

    Updates

---
+++ path---index-1e07658c1901603b4ff2.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{324:function(e,t){e.exports={data:{allMarkdownRemark:{edges:[{node:{excerpt:"Oooooh-weeee, my first blog post!",id:"C:/Users/michal.gebauer/Documents/GitHub/michalgebauer.github.io/src/pages/10-02-2018-first-test/index.md absPath of file >>> MarkdownRemark",frontmatter:{title:"My First Gatsby Post",date:"February 10, 2018",path:"/first-test"}}}]}},pathContext:{}}}});
+//# sourceMappingURL=path---index-1e07658c1901603b4ff2.js.map
