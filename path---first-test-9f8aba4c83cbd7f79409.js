de3c03cf20fbd7c04e0f56533fa9c9690515bee5
michalgebauer <michal.gebauer@gmail.com>
2018-02-12 08:38:16 +0100

    Updates

--- path---first-test-9f8aba4c83cbd7f79409.js
+++ path---first-test-9f8aba4c83cbd7f79409.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xc716229000fb],{323:function(t,e){t.exports={data:{markdownRemark:{html:"<p>Oooooh-weeee, my first blog post!</p>",frontmatter:{date:"February 10, 2018",path:"/first-test",title:"My First Gatsby Post"}}},pathContext:{}}}});
+webpackJsonp([0xc716229000fb],{563:function(t,e){t.exports={data:{markdownRemark:{html:"<p>Oooooh-weeee, my first blog post!</p>",frontmatter:{date:"February 10, 2018",path:"/first-test",title:"My First Gatsby Post"}}},pathContext:{}}}});
 //# sourceMappingURL=path---first-test-9f8aba4c83cbd7f79409.js.map
